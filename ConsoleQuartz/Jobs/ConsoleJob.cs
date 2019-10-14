f07c198044c799801bcaedc13530350e4bfc8df2
Goal-developer <1506017918@qq.com>
2019-10-14 22:26:19 +0800

    控制台基于Quartz.Net组件实现定时任务调度

---
+++ ConsoleQuartz/Jobs/ConsoleJob.cs
@@ -0,0 +1,21 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleQuartz
+{
+    /// <summary>
+    /// 实现IJob,Execute方法里编写要处理的业务逻辑
+    /// </summary>
+    public class ConsoleJob : IJob
+    {
+        public Task Execute(IJobExecutionContext context)
+        {
+            return Console.Out.WriteLineAsync($"ConsoleJob执行工作 在当前时间{DateTime.Now}--上一次执行时间：{DateTime.Now.AddSeconds(-5)}");
+        }
+    }
+}
---
+++ ConsoleQuartz/Program.cs
@@ -0,0 +1,53 @@
+﻿using Quartz;
+using Quartz.Impl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleQuartz
+{
+    class Program
+    {
+        /// <summary>
+        /// 入口服务
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Run();
+            Console.ReadKey();
+        }
+
+
+        /// <summary>
+        /// 任务调度的使用过程
+        /// </summary>
+        /// <returns></returns>
+        public async static Task Run()
+        {
+            // 1.创建scheduler的引用
+            ISchedulerFactory schedFact = new StdSchedulerFactory();
+            IScheduler sched = await schedFact.GetScheduler();
+            
+
+            //2.启动 scheduler
+            await sched.Start();
+
+            // 3.创建 job
+            IJobDetail job = JobBuilder.Create<ConsoleJob>()
+                    .WithIdentity("job1", "group1")
+                    .Build();
+
+            // 4.创建 trigger （创建 trigger 触发器）
+            ITrigger trigger = TriggerBuilder.Create()
+                .WithIdentity("trigger1", "group1")  //触发器 组
+                .WithSimpleSchedule(x => x.WithIntervalInSeconds(5).RepeatForever())
+                .Build();
+
+            // 5.使用trigger规划执行任务job （使用触发器规划执行任务）
+            await sched.ScheduleJob(job, trigger);
+        }
+    }
+}
